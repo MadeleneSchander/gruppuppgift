e8352208ee4b61d4163340c54600798c51323069
MadeleneSchander <56534319+MadeleneSchander@users.noreply.github.com>
2019-10-24 19:15:38 +0200

    all stuff

--- script.js
+++ script.js
@@ -1,51 +1,22 @@
-var quotes = [
-	"“Wisdom is not a product of schooling but of the lifelong attempt to acquire it.”  <br><br>— Albert Einstein",
-	"“Live as if you were to die tomorrow. Learn as if you were to live forever.” <br><br>—  Mahatma Gandhi",
-	"“Tell me and I forget, teach me and I may remember, involve me and I learn.” <br><br>— Benjamin Franklin",
-	"“Study hard what interests you the most in the most undisciplined, irreverent and original manner possible.”<br><br>— Richard Feynman",
-	
-	"“It is important that students bring a certain ragamuffin, barefoot irreverence to their studies; they are not here to worship what is known, but to question it.”  <br><br>—  Jacob Bronowski"
-];
+// Tobias kod - Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-var index = 0;
-var max = quotes.length - 1;
-var delay = .02;
-
-function random(min, max){
-	return (Math.random() * (max - min)) + min;
-}
-
-function cycleQuotes(arr, i, sel){
-	var el = $(sel);
-	var message = arr[i];
-	el.html(message);
-	var split = new SplitText(el);
-	var time = split.chars.length * delay;
-	
-	$(split.chars).each(function(i){
-		TweenMax.from($(this), time, {
-			opacity: 0,
-			x: 0,
-			y: random(-200, 200),
-			z: random(500, 1000),
-			// scale: .1,
-			delay: i * delay,
-			yoyo: true,
-			repeat: -1,
-			repeatDelay: time * 4,
-			ease: Power1.easeOut
-		});
-	});
-	
-	index = index == max ?  0 : (index + 1);
-
-	setTimeout(function(){
-		
-		cycleQuotes(quotes, index, ".split");
-	}, ((time * 4) + (time * 4)) * 1000);
-	
-}
-
-$(window).load(function(){
-	cycleQuotes(quotes, index, ".split");
-});
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
+  
